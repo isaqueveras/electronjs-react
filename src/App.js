11669e7fcfe99519782f32881815a807741dbe53
isaqueveras <isaque.verasccb@gmail.com>
2022-03-09 11:12:29 -0300

    Adicionando o sidebar utilizando tailwind css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
+import SideBar from "./components/sidebar";
+
 function App() {
   return (
-    <div className="bg-rose-700 w-full h-full flex-1">
-      <div className="container bg-slate-200 items-center ml-auto mr-auto">
-        <h1 className="text-3xl font-bold">
-          Hello world!
-        </h1>
-      </div>
+    <div className="w-full min-h-screen bg-slate-50">
+      <SideBar />
     </div>
   );
 }
